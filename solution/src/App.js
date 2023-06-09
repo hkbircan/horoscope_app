7ceb3111433ffac14a9c7b01ad24a00edc5488b6
Kubilay <hkbircan@gmail.com>
2023-06-09 10:41:05 +0200

    first commit

---
+++ solution/src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Main />
+
+    </div>
+  );
+}
+
+export default App;
